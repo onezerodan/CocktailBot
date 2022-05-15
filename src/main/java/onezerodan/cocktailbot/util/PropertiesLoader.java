8266bca8f522fb53b02316cebc5deecdb95f17ec
onezerodan <danielpechurin@gmail.com>
2022-05-16 02:34:56 +0500

    added props for parser

---
+++ src/main/java/onezerodan/cocktailbot/util/PropertiesLoader.java
@@ -0,0 +1,31 @@
+package onezerodan.cocktailbot.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertiesLoader {
+
+    static Logger log = LoggerFactory.getLogger(PropertiesLoader.class);
+    public  Properties getProperties() {
+
+        String propPath;
+        Properties properties = new Properties();
+        try {
+            propPath = PropertiesLoader.class.getClassLoader().getResource("parser.properties").getPath();
+            properties.load(new FileInputStream(propPath));
+        } catch (NullPointerException | IOException e) {
+            log.error("Properties for parser not found.");
+            return null;
+        }
+        return properties;
+
+
+
+
+
+    }
+}
