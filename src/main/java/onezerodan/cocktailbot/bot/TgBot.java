fa4e907f18f862cc1050c68992713b03f6417788
onezerodan <danielpechurin@gmail.com>
2022-05-23 01:20:36 +0500

    added suggestions if not found to bot

--- src/main/java/onezerodan/cocktailbot/bot/TgBot.java
+++ src/main/java/onezerodan/cocktailbot/bot/TgBot.java
@@ -1,5 +1,6 @@
 package onezerodan.cocktailbot.bot;
 
+import onezerodan.cocktailbot.model.Cocktail;
 import onezerodan.cocktailbot.service.CocktailService;
 import onezerodan.cocktailbot.util.PropertiesLoader;
 import org.slf4j.Logger;
@@ -7,13 +8,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.Properties;
 
 public class TgBot extends TelegramLongPollingBot {
@@ -44,17 +43,44 @@ public class TgBot extends TelegramLongPollingBot {
         String text = update.getMessage().getText().toLowerCase();
 
         if (text.startsWith("/search")) {
-
-            sendMessage(chatId, searchCocktailByName(text.replace("/search ", "").replace("ё", "е")));
+            searchCocktailByName(text.replace("/search ", "").replace("ё", "е"), chatId);
         }
         if (text.startsWith("/ingr")) {
             sendMessage(chatId, searchCocktailByIngredients(text.replace("/ingr ", "").replace("ё", "е")));
         }
-        sendMessage(chatId, text);
+
     }
 
-    private String searchCocktailByName(String name) {
-        return cocktailService.findByName(name).toString();
+
+    private void searchCocktailByName(String name, Long chatId) {
+
+        List<Cocktail> cocktails = cocktailService.findByName(name);
+        if (cocktails.size() == 1) {
+            for (Cocktail cocktail : cocktails) {
+                sendMessage(chatId, cocktail.toString());
+            }
+        }
+        else if (cocktails.size() > 1) {
+            sendMessage(chatId,"По вашему запросу найдено несколько коктейлей:");
+            for (Cocktail cocktail : cocktails) {
+                sendMessage(chatId, cocktail.getName());
+            }
+        }
+
+        else {
+
+            List<Cocktail> suggestions = cocktailService.suggestIfNotFound(name);
+            if (suggestions.size() > 0) {
+                sendMessage(chatId,"Возможно, вы имели ввиду: ");
+                for (Cocktail cocktail : suggestions) {
+                    sendMessage(chatId, cocktail.getName());
+                }
+            }
+            else {
+                sendMessage(chatId, "По вашему запросу ничего не найдено.");
+            }
+        }
+
     }
     private String searchCocktailByIngredients(String ingredients) {
         List<String> ingredientsList = Arrays.asList(ingredients.split(", "));
